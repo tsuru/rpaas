48a51e35719a759d34d1de81997cc28f679797ba
Wagner Souza <wagnersza@corp.globo.com>
2015-01-16 15:38:29 -0200

    flower: basic auth

--- runner.sh
+++ runner.sh
@@ -5,7 +5,7 @@ case $RPAAS_ROLE in
         celery -A rpaas.tasks worker
         ;;
     "flower")
-        celery flower -A rpaas.tasks --address=0.0.0.0 --port=$PORT
+        celery flower -A rpaas.tasks --address=0.0.0.0 --port=$PORT --basic_auth=$FLOWER_USER:$FLOWER_PASSWORD
         ;;
     *)
         gunicorn rpaas.api:api -b 0.0.0.0:$PORT
