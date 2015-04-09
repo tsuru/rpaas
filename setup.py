18fe2b6356c73a87e64ce38db62a92d08c0f99b6
Cezar Sa Espinola <cezarsa@gmail.com>
2015-04-09 15:42:27 -0300

    increment tsuru-hm version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "Flask==0.9",
         "requests==2.4.3",
         "gunicorn==0.17.2",
-        "tsuru-hm==0.2.1",
+        "tsuru-hm==0.2.2",
         "celery[redis]",
         "flower==0.7.3",
         "GloboNetworkAPI==0.2.2",
