4b87e77025c77e0679fdd290a3a8f6c41733ef2a
Francisco Souza <fss@corp.globo.com>
2015-10-16 11:50:09 -0300

    runner: send access log to stdout

--- runner.sh
+++ runner.sh
@@ -8,6 +8,6 @@ case $RPAAS_ROLE in
         celery flower -A rpaas.tasks --address=0.0.0.0 --port=$PORT --basic_auth=$FLOWER_USER:$FLOWER_PASSWORD
         ;;
     *)
-        gunicorn rpaas.api:api -b 0.0.0.0:$PORT
+        gunicorn rpaas.api:api -b 0.0.0.0:$PORT --access-logfile -
         ;;
 esac
