709fae8f990fce6fb5abc67a0565c275f7e5d3ef
Francisco Souza <fss@corp.globo.com>
2015-12-11 14:46:53 -0200

    all: add copyright information
    It was absent in some files.

--- rpaas/ssl_plugins/__init__.py
+++ rpaas/ssl_plugins/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# Copyright 2015 rpaas authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 from abc import ABCMeta, abstractmethod
 
 
--- rpaas/ssl_plugins/le_authenticator.py
+++ rpaas/ssl_plugins/le_authenticator.py
@@ -1,4 +1,7 @@
-"""RPAAS plugin."""
+# Copyright 2015 rpaas authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 import logging
 import pipes
 import time
