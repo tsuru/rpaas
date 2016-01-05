702d1f926f5679bac410eb85d0db80de82769803
Francisco Souza <fss@corp.globo.com>
2016-01-05 10:48:20 -0200

    le_renewer: use optmistic locking
    This is safer, now we can restart the process and don't fight starvation
    :-)

--- rpaas/ssl_plugins/le_renewer.py
+++ rpaas/ssl_plugins/le_renewer.py
@@ -2,15 +2,16 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+import datetime
 import os
 import threading
 import time
 
 import redis
 
-from redis import lock
-
 from rpaas import tasks
+
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
 class LeRenewer(threading.Thread):
@@ -27,19 +28,35 @@ class LeRenewer(threading.Thread):
         self.daemon = True
         self.config = config or os.environ
         self.interval = int(self.config.get("LE_RENEWER_RUN_INTERVAL", 86400))
-        lock_name = self.config.get("LE_RENEWER_LOCK_NAME", "le_renewer_rpaas")
-        conn = redis.StrictRedis(host=tasks.redis_host, port=tasks.redis_port,
-                                 password=tasks.redis_password)
-        self.lock = lock.Lock(conn, lock_name, timeout=self.interval*2,
-                              sleep=self.interval / 24 + 1)
+        self.last_run_key = self.config.get("LE_RENEWER_LAST_RUN_KEY", "le_renewer:last_run")
+        self.conn = redis.StrictRedis(host=tasks.redis_host, port=tasks.redis_port,
+                                      password=tasks.redis_password)
 
     def run(self):
         self.running = True
         while self.running:
-            self.lock.acquire()
-            tasks.RenewCertsTask().delay(self.config)
+            if self.try_lock():
+                tasks.RenewCertsTask().delay(self.config)
             time.sleep(self.interval)
-            self.lock.release()
+
+    def try_lock(self):
+        interval_delta = datetime.timedelta(seconds=self.interval)
+        with self.conn.pipeline() as pipe:
+            try:
+                now = datetime.datetime.utcnow()
+                pipe.watch(self.last_run_key)
+                last_run = pipe.get(self.last_run_key)
+                if last_run:
+                    last_run_date = datetime.datetime.strptime(last_run, DATETIME_FORMAT)
+                    if now - last_run_date < interval_delta:
+                        pipe.unwatch(self.last_run_key)
+                        return False
+                pipe.multi()
+                pipe.set(self.last_run_key, now.strftime(DATETIME_FORMAT))
+                pipe.execute()
+                return True
+            except redis.WatchError:
+                return False
 
     def stop(self):
         self.running = False
