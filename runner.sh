b3336c4edfc5c85e06a03e472a2cc5515e08014e
Paulo Sousa <paulo.sousa@corp.globo.com>
2017-04-27 16:00:07 -0300

    runner: add WORKERS var for gunicorn

--- runner.sh
+++ runner.sh
@@ -8,6 +8,6 @@ case $RPAAS_ROLE in
         celery flower -A rpaas.tasks --address=0.0.0.0 --port=$PORT --basic_auth=$FLOWER_USER:$FLOWER_PASSWORD
         ;;
     *)
-        gunicorn rpaas.api:api -b 0.0.0.0:$PORT --access-logfile - -k gevent
+        gunicorn rpaas.api:api -b 0.0.0.0:$PORT --access-logfile - -w ${WORKERS:=1} -k gevent
         ;;
 esac
